2755b560b67856c1bded2ca367194fce53301da2
jonathan.xu <13804266693@163.com>
2019-08-15 18:19:09 +0800

    product server cluster, region and zone, logback, feign client, ribbon retry, ribbon load balance, hystrix fuse

--- product-server/src/main/java/com/xufq/productserver/client/UserFeignClient.java
+++ product-server/src/main/java/com/xufq/productserver/client/UserFeignClient.java
@@ -1,7 +1,9 @@
 package com.xufq.productserver.client;
 
+import com.xufq.productserver.client.config.UserFeignClientConfiguration;
 import com.xufq.productserver.vo.UserVo;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -12,9 +14,22 @@ import org.springframework.web.bind.annotation.PathVariable;
  * @Date 8/1/2019 09:37 PM
  * @Version 1.0
  */
-@FeignClient(name = "user-server", path = "/user-server")
+@FeignClient(name = "user-server", path = "/user-server", fallback = UserFeignClient.UserFeignClientFallback.class, configuration = UserFeignClientConfiguration.class)
 public interface UserFeignClient {
 
     @GetMapping("/user/{userId}")
     UserVo getUserById(@PathVariable String userId);
+
+    @Component
+    class UserFeignClientFallback implements UserFeignClient{
+
+        @Override
+        public UserVo getUserById(String userId){
+            return UserVo.builder()
+                    .userId(userId)
+                    .userName("FallbackUserName")
+                    .build();
+        }
+    }
+
 }
